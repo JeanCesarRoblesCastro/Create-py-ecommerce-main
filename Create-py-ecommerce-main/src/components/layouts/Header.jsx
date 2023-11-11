5d4fe19a10a38f0b93a6f72c0cf165313297763d
JeanCesarRoblesCastro <jcesarroblescastro01@gmail.com>
2023-11-11 05:07:47 -0500

    first commit

---
+++ Create-py-ecommerce-main/src/components/layouts/Header.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+
+import Cart from "../cart/Cart";
+
+const Header = ({ title }) => {
+    return (
+        <header className="fixed z-20 top-0 left-0 w-full py-4 bg-zinc-900/50" id="header">
+            <div className="container px-4 mx-auto flex items-center justify-between">
+                <h1 className="text-yellow-500 text-2xl font-black">{title}</h1>
+                <img src="/android-chrome-192x192.png" width="40px" height="30px" />
+                <a href="#cartOffCanvas" className="text-xl">🛒</a>
+                <Cart />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
